1ef09481a3b2401ffe3d6c6752e1be2956a277b9
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-03-09 19:44:42 +0100

    refactor TTable class
    The class wasn't readable enough, so the code was simplified.

--- src/commonMain/kotlin/com/github/pksokolowski/nrg/engine/search/transposition/TTable.kt
+++ src/commonMain/kotlin/com/github/pksokolowski/nrg/engine/search/transposition/TTable.kt
@@ -3,8 +3,9 @@ package com.github.pksokolowski.nrg.engine.search.transposition
 import com.github.pksokolowski.nrg.engine.GameState
 
 @ExperimentalUnsignedTypes
-class TTable(private val hashMaker: HashMaker, val length: Int) {
+class TTable(private val hashMaker: HashMaker, length: Int) {
     private val data = Array<TTableEntry?>(length) { null }
+    private val length = length.toULong()
 
     operator fun get(gameState: GameState): TTableEntry {
         val hash = hashMaker.hashOf(gameState)
@@ -14,19 +15,13 @@ class TTable(private val hashMaker: HashMaker, val length: Int) {
             ?: create(index, hash)
     }
 
-    private fun matchOrNull(index: Int, hash: ULong): TTableEntry? {
-        val entry = data[index] ?: return null
-        return if (entry.hash == hash) entry else null
+    private fun matchOrNull(index: Int, hash: ULong) = data[index]?.let {
+        if (it.hash == hash) it else null
     }
 
-    private fun create(index: Int, hash: ULong) =
-        TTableEntry.getEmpty(hash).also {
-            data[index] = it
-            return it
-        }
-
-    private fun indexOf(hash: ULong): Int {
-        val index = hash % length.toULong()
-        return index.toInt()
+    private fun create(index: Int, hash: ULong) = TTableEntry.getEmpty(hash).also {
+        data[index] = it
     }
+
+    private fun indexOf(hash: ULong) = (hash % length).toInt()
 }
