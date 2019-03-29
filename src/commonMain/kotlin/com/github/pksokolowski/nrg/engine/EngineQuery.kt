5fc1adcf3287b0a1f2e960c2f711b823e5abbcaf
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-03-29 13:03:22 +0100

    set default max depth to 30
    When max depth was not specified, default value was Int.MAX_VALUE. This
    is problematic because KillerMove heuristic internally creates an array
    of the max depth size.

--- src/commonMain/kotlin/com/github/pksokolowski/nrg/engine/EngineQuery.kt
+++ src/commonMain/kotlin/com/github/pksokolowski/nrg/engine/EngineQuery.kt
@@ -3,5 +3,5 @@ package com.github.pksokolowski.nrg.engine
 class EngineQuery(val state: GameState,
                   val chosenMove: Move,
                   val timeAllowed: Long? = null,
-                  val depthAllowed: Int = Int.MAX_VALUE,
+                  val depthAllowed: Int = 30,
                   val randomize: Boolean = true)
