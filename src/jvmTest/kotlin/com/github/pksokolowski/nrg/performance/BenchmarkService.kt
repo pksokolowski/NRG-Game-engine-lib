2c572b1a317d5cf38b2e80bd138fa4e72499b325
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-04-20 23:23:35 +0200

    simplify performance testing
    Since queryProcessor no longer takes a Move parameter, instead expecting
    it to be already applied to GameState passed as another parameter, it
    made sense to remove the redundant explicit move information from the
    test cases. The exact last move doesn't make a difference to the engine
    anymore - it only receives the exact state passed to it.
    Move counters were incremented by 1 to reflect the fact the moves were
    already applied.

--- src/jvmTest/kotlin/com/github/pksokolowski/nrg/performance/BenchmarkService.kt
+++ src/jvmTest/kotlin/com/github/pksokolowski/nrg/performance/BenchmarkService.kt
@@ -1,123 +1,67 @@
 package com.github.pksokolowski.nrg.performance
 
 import com.github.pksokolowski.nrg.engine.EngineQuery
-import com.github.pksokolowski.nrg.engine.Move
 import com.github.pksokolowski.nrg.engine.utils.toGameState
 
 class BenchmarkService {
 
     val queryPreparers = listOf(
-        ::prepareGameStartQuery,
-        ::prepareMidGameQuery,
-        ::prepareMidGameDenseQuery,
-        ::prepareMidGameFarApartQuery,
-        ::prepareLateGameQuery,
-        ::prepareDispersedLateGameQuery,
-        ::prepareBattleOf2sQuery,
-        ::prepareClinchCase
-    )
-
-    class QueryData(val query: EngineQuery, val info: String)
-
-    fun prepareGameStartQuery(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             +1 +1 +1 +1 +1 +1 +1 +1
             +1 +1 +1 +1 +1 +1 +1 +1
             00 00 00 00 00 00 00 00
             00 00 00 00 00 00 00 00
             00 00 00 00 00 00 00 00
-            00 00 00 00 00 00 00 00
+            00 00 00 00 -1 00 00 00
+            -1 -1 -1 -1 00 -1 -1 -1
             -1 -1 -1 -1 -1 -1 -1 -1
-            -1 -1 -1 -1 -1 -1 -1 -1
-        """.toGameState()
+        """.toQueryData(1, depth, "[realistic load] Game beginning."),
 
-        val move = Move(-1, 4, 6, 4, 5, 0)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(
-            query,
-            "[realistic load] Game beginning. (depth = $depth)"
-        )
-    }
-
-    fun prepareMidGameQuery(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             00 +1 00 00 00 00 00 00
             +1 00 00 00 00 00 00 00
             00 00 00 00 00 +4 00 00
-            00 +3 00 00 +2 +1 00 00
-            00 -2 00 -3 +1 +3 00 +2
+            00 +3 00 00 -4 +1 00 00
+            00 -2 00 00 +1 +3 00 +2
             00 00 -2 00 00 00 +3 00
             -1 -1 00 00 -3 -2 -5 -4
             00 00 -1 00 00 00 00 00
-        """.toGameState(10)
+        """.toQueryData(11, depth, "Mid-game."),
 
-        val move = Move(-3, 3, 4, 4, 3, 2)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(query, "Mid-game. (depth = $depth)")
-    }
-
-    fun prepareMidGameDenseQuery(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             00 00 00 00 00 00 00 00
             00 00 00 00 00 00 00 00
             00 00 00 00 00 +4 00 00
-            00 +3 00 +3 +2 +1 00 00
-            00 -2 00 -3 +1 +3 00 +2
+            00 +3 00 +3 -4 +1 00 00
+            00 -2 00 00 +1 +3 00 +2
             00 00 -2 -3 00 -2 +3 00
             -3 -3 00 00 -3 -2 -5 -4
             +5 00 -1 00 00 +5 00 +2
-        """.toGameState(10)
+        """.toQueryData(11, depth, "Mid game, dense."),
 
-        val move = Move(-3, 3, 4, 4, 3, 2)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(
-            query,
-            "Mid game, dense. (depth = $depth)"
-        )
-    }
-
-    fun prepareMidGameFarApartQuery(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             00 00 00 00 00 00 00 00
             00 00 +3 00 00 +4 00 00
             00 +1 +1 00 +3 00 +4 +3
             00 00 +3 00 00 00 00 00
-            00 -2 00 00 +1 00 00 +2
-            -4 -2 -1 -3 -2 -2 -2 -5
+            00 -2 00 00 -3 00 00 +2
+            -4 -2 -1 -3 -2 00 -2 -5
             00 00 -2 00 00 -2 +3 00
             00 00 00 00 00 00 00 00
-        """.toGameState(10)
+        """.toQueryData(11, depth, "Mid game, pieces far apart."),
 
-        val move = Move(-2, 5, 5, 4, 4, 1)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(
-            query,
-            "Mid game, pieces far apart. (depth = $depth)"
-        )
-    }
-
-    fun prepareLateGameQuery(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             00 00 00 00 00 00 00 00
             00 00 00 00 00 00 00 00
             00 00 00 00 00 00 00 00
             00 00 00 +5 00 00 00 -3
-            00 +4 00 00 +3 +2 00 00
-            00 00 00 -2 00 00 00 -2
+            00 +4 00 00 -4 +2 00 00
+            00 00 00 00 00 00 00 -2
             00 00 +2 -4 00 00 -1 00
             00 00 00 00 00 -4 00 00
-        """.toGameState(10)
+        """.toQueryData(11, depth, "[realistic load] Late game."),
 
-        val move = Move(-2, 3, 5, 4, 4, 3)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(
-            query,
-            "[realistic load] Late game (depth = $depth)"
-        )
-    }
-
-    fun prepareDispersedLateGameQuery(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             00 00 00 00 00 +3 00 00
             00 00 00 +5 00 00 00 00
             00 +4 00 00 00 +2 00 00
@@ -125,54 +69,37 @@ class BenchmarkService {
             -4 00 -2 00 00 00 00 00
             00 00 00 00 00 -3 00 00
             00 00 00 -1 00 00 00 -2
-            +2 00 00 00 00 -4 00 00
-        """.toGameState(10)
+            +2 00 00 -4 00 00 00 00
+        """.toQueryData(11, depth, "[realistic load] Late game with pieces dispersed."),
 
-        val move = Move(-4, 5, 7, 3, 7, 0)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(
-            query,
-            "[realistic load] Late game with pieces dispersed (depth = $depth)"
-        )
-    }
-
-    fun prepareBattleOf2sQuery(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             00 00 00 00 00 00 00 00
             00 00 00 00 00 00 00 00
             00 00 +2 +2 +2 +2 00 00
-            00 00 +2 +2 +2 +2 00 00
-            00 00 -2 -2 -2 -2 00 00
+            00 00 +2 -4 +2 +2 00 00
+            00 00 00 -2 -2 -2 00 00
             00 00 -2 -2 -2 -2 00 00
             00 00 00 00 00 00 00 00
             00 00 00 00 00 00 00 00
-        """.toGameState(20)
+        """.toQueryData(21, depth, "[realistic load] Two rectangles of 2-energy pieces, zero distance."),
 
-        val move = Move(-2, 2, 4, 3, 3, 2)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(
-            query,
-            "[realistic load] Two rectangles of 2-energy pieces, zero distance (depth = $depth)"
-        )
-    }
-
-    fun prepareClinchCase(depth: Int): QueryData {
-        val state = """
+        fun(depth: Int) = """
             +1 00 +1 00 +1 +1 +1 00
             +1 +1 00 +1 00 +1 00 +1
             +1 00 +1 00 +1 00 +1 00
             -1 +1 00 +1 00 00 -1 00
             00 -1 00 -1 00 -1 00 -1
             -1 00 -1 00 -1 00 -1 00
-            00 -1 00 -1 00 -1 00 -1
-            00 00 00 00 -1 00 -1 00
-        """.toGameState(20)
+            00 -1 00 -1 -1 -1 00 -1
+            00 00 00 00 00 00 -1 00
+        """.toQueryData(21, depth, "[realistic load] Near beginning. No captures available without sacrifice.")
+    )
 
-        val move = Move(-1, 4, 7, 4, 6, 2)
-        val query = EngineQuery(state.withMove(move), depthAllowed = depth, randomize = false)
-        return QueryData(
-            query,
-            "[realistic load] Near beginning. No captures available without sacrifice (depth = $depth)"
-        )
+    class QueryData(val query: EngineQuery, val info: String)
+
+    private fun String.toQueryData(movesCount: Int, depth: Int, description: String): QueryData {
+        val state = this.toGameState(movesCount)
+        val query = EngineQuery(state, depthAllowed = depth, randomize = false)
+        return QueryData(query, "$description (depth = $depth)")
     }
 }
